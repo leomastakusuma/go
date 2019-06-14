0a55b7bf19a9a301ea5a6a6c2633dc56ef55c046
Leo Masta <leomastakusuma@gmail.com>
2019-06-14 15:20:49 +0700

    update

--- controller/profile.go
+++ controller/profile.go
@@ -1,19 +1,19 @@
 package Controller
 
 import (
-		"net/http"
-		"github.com/gorilla/mux"
-		"encoding/json"
-		"../library"
-		"../structs"
-		"log"
-		_ "github.com/go-sql-driver/mysql"
+	"encoding/json"
+	"log"
+	"net/http"
 
-
+	"../library"
+	"../structs"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
 func Routers(r *mux.Router) {
-    r.HandleFunc("/user/e", Myprofile).Methods("GET")
+	r.HandleFunc("/user", Myprofile).Methods("GET")
+	r.HandleFunc("/user", createUser).Methods("POST")
 }
 
 func Myprofile(w http.ResponseWriter, r *http.Request) {
@@ -27,7 +27,6 @@ func Myprofile(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Print(err)
 	}
-
 	for rows.Next() {
 		if err := rows.Scan(&users.Id, &users.FirstName, &users.LastName); err != nil {
 			log.Fatal(err.Error())
@@ -35,17 +34,43 @@ func Myprofile(w http.ResponseWriter, r *http.Request) {
 		} else {
 			arr_user = append(arr_user, users)
 		}
-
 	}
-
 	response.Status = true
 	response.Message = "Success"
-	response.Data = arr_user 
-
+	response.Data = arr_user
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
 }
 
+func createUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	db := library.GetDB()
+	defer db.Close()
+	stmt, err := db.Prepare("INSERT INTO person(first_name,last_name) VALUES(?,?)")
+	if err != nil {
+		panic(err.Error())
+	}
+	var user structs.Users
+	var arr_user []structs.Users
+	var response structs.Response
 
+	errs := json.NewDecoder(r.Body).Decode(&user)
+	arr_user = append(arr_user, user)
+	if errs != nil {
+		panic(err.Error())
+	}
 
+	firstName := user.FirstName
+	lastName := user.LastName
+	_, err = stmt.Exec(firstName, lastName)
+	if err != nil {
+		panic(err.Error())
+	}
+	response.Status = true
+	response.Message = "Success"
+	response.Data = arr_user
+
+	json.NewEncoder(w).Encode(response)
+
+}
--- model/Users.go
+++ model/Users.go
@@ -1,15 +1,13 @@
-
 package Model
 
 type Users struct {
-	Id        bool `form:"id" json:"id"`
+	Id        bool   `form:"id" json:"id"`
 	FirstName string `form:"firstname" json:"firstname"`
 	LastName  string `form:"lastname" json:"lastname"`
 }
 
 type Response struct {
-	Status  string  `json:"status"`
+	Status  string `json:"status"`
 	Message string `json:"message"`
 	Data    Users
 }
-
