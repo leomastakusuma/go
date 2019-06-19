10687c61978ea1908d99dda00200df777c44b0f1
Leo Masta <leomastakusuma@gmail.com>
2019-06-19 15:06:25 +0700

    update

--- library/connect.go
+++ library/connect.go
@@ -7,7 +7,7 @@ import (
 
 var db *sql.DB
 func GetDB() *sql.DB {
-	db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/test")
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/test")
 	if err != nil {
 		panic(err)
 	}
